a5d43b02ceb4073ef4e6d485f21eeb26ffab959a
Niall O'Callaghan <noccer@noccer-MacBook.local>
2016-05-09 19:20:24 +1000

    the app is pretty much working, just need to tidy up game logic. CHANGES>> style.css: added in .playerTwoClicked style app.js: [deep breath] lots of commenting, lots of renaming variables into more logical names, updated makeGameArea row and column classes to be more usable i.e. r0c2. added in functionality that picks out the class of the item clicked, adds it to a variable (a string), then split it into an array which I used to index the row and column, which I then feed into the playerMove function.

--- js/app.js
+++ js/app.js
@@ -13,33 +13,36 @@ var gridSize = 3; ///this will determine the grid size
 var playerOne = ["Player 1", 0, "X"]; ///name, score & symbol
 var playerTwo = ["Player 2", 0, "O"]; ///name, score & symbol
 
-var defaultBlankSymbol = "_";
-var defaultBlankSymbolString = "";
-var createDefaultBlankSymbolString = function(){
+var whoIsPlayingNow = playerOne; ///check whose turn it is
+console.log("whoIsPlayingNow = "+whoIsPlayingNow[0]);
+
+var emptyCellSymbol = "_"; ///this is just to set a default value. I might use this value in the front end at some point.
+var emptyCellSymbolString = "";///variable length string that we split into an array later to populate the rowsInput arrays
+var createEmptyCellSymbolString = function(){
 	for (var i = 0; i < gridSize; i++) {
-		defaultBlankSymbolString+=defaultBlankSymbol;
-	}
-};
-createDefaultBlankSymbolString();
+		emptyCellSymbolString+=emptyCellSymbol;
+	}///close for loop
+};///close createEmptyCellSymbolString
+createEmptyCellSymbolString();
 
 ///manually inputting rows for now, but hope to make these automatic
-var rowOneInput = defaultBlankSymbolString.split("");
+var rowOneInput = emptyCellSymbolString.split("");///make an array from emptyCellSymbolString >>> if gridSize is 3, rowOneInput => ['_','_','_']
 var rowOneValues = [1,2,4];
 
-var rowTwoInput = defaultBlankSymbolString.split("");
+var rowTwoInput = emptyCellSymbolString.split("");
 var rowTwoValues = [8,16,32];
 
-var rowThreeInput = defaultBlankSymbolString.split("");
+var rowThreeInput = emptyCellSymbolString.split("");
 var rowThreeValues = [64,128,256];
 
-var winningScores = [7,56,448,73,146,292,273,84];
+var winningScores = [7,56,448,73,146,292,273,84];///these are the scores that will return a winning score hit. Because each 'cell' is worth double the value of its predecessor, we can do a CHECKSUM on the results to see if somebody won.
 var playerScoreThisRound = 0;
 
-var allInputsArray = [rowOneInput,rowTwoInput,rowThreeInput];
-var allValuesArray = [rowOneValues,rowTwoValues,rowThreeValues];
+var allInputsArray = [rowOneInput,rowTwoInput,rowThreeInput]; ///nest array
+var allValuesArray = [rowOneValues,rowTwoValues,rowThreeValues]; ///nest array
 
-var playerMove = function(player, rowNum,colNum/*,letter*/){
-	if (allInputsArray[rowNum][colNum] === defaultBlankSymbol){
+var playerMove = function(player,rowNum,colNum){ ///update arguments
+	if (allInputsArray[rowNum][colNum] === emptyCellSymbol){
 		allInputsArray[rowNum].splice(colNum,1,player[2]);
 	}
 	console.table(allInputsArray);
@@ -47,7 +50,16 @@ var playerMove = function(player, rowNum,colNum/*,letter*/){
 	playerScoreThisRound = (allValuesArray[rowNum][colNum])
 	console.log(player[0]+" current score is: "+player[1]);
 	checkWhoWon(player);
-};
+	console.log(whoIsPlayingNow[0]+ " has just taken a turn");
+	///check who has been playing this round then swap to other player
+	if (whoIsPlayingNow[0] === playerOne[0]) {
+		whoIsPlayingNow = playerTwo;
+	}
+	else {
+		whoIsPlayingNow = playerOne;
+	}
+	console.log(whoIsPlayingNow[0]+ " is up next");
+};///close playerMove
 
 var checkWhoWon = function(player){
 	for (var i = 0; i < winningScores.length; i++) {
@@ -56,28 +68,49 @@ var checkWhoWon = function(player){
 			console.log(player[0]+" won!");
 		}
 		playerScoreThisRound = 0;
-	}
-};
+	}///close for loop
+};///close checkWhoWon
 
 var makeGameArea = function (){
 	var gameOutline = $('<div>').attr('id','gameOutline');
 	gameOutline.appendTo($('#gameSection'));
 	///make rows
 	for (var i = 0; i < gridSize; i++) {
-		var rows = $('<div>').addClass('row '+'row'+(i+1));
-		rows.appendTo($('#gameOutline'));
+		var rows = $('<div>')
+		.addClass('row '+'row'+(i));//add row classes
+		rows.appendTo($('#gameOutline'));///pass into webpage
 		///make columns
 		for (var j = 0; j < gridSize; j++) {
-			var cols = $('<div>').addClass('col '+'col'+(j+1));
-			cols.appendTo($('.row'+(i+1)));
-		}
-	}
-}
+			var cols = $('<div>') ///create divs
+			///KEY STEP: give columns unique identifiers!
+			///Format: rowNumberColNumber, shortened to r#c#
+			///this will be split into an array later on and used to detect which column/row the click was upon. This makes the game scaleable.
+			.addClass(('r'+(i))+'c'+(j)+' col');
+			cols.appendTo($('.row'+(i))); ///apply
+		}///close 'j' for loop
+	}///close 'i' for loop
+};///close makeGameArea
 makeGameArea();
 
 $('.col').on('click', function(){
-  $(this)
-    .addClass('playerOneClicked');
+	console.log("here are the classes from your click");
+	var clickedBoxClassList = ($(this).attr("class"));///returns string of  the classes that were clicked
+	console.log(clickedBoxClassList);
+	var clickedBoxClassListArray = clickedBoxClassList.split(""); ///split the original classes string into an array which we can now index, with index[1] being the row value, and index[3] being the column value
+	console.log(clickedBoxClassListArray);
+	var rowClicked = clickedBoxClassListArray[1];///make playerMove more readable!
+	var colClicked = clickedBoxClassListArray[3];///make playerMove more readable!
+
+	playerMove(whoIsPlayingNow,rowClicked,colClicked);
+
+	if (whoIsPlayingNow === playerOne){
+		$(this)
+		.addClass('playerTwoClicked');
+	}
+	else {
+		$(this)
+		.addClass('playerOneClicked');
+	}
 });
 
 //////////////////////////////////
