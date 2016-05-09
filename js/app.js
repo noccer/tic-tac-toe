0049c97ee1c44bb5146d6d0652dbf6ded8ddae73
Niall O'Callaghan <noccer@noccer-MacBook.local>
2016-05-09 16:11:11 +1000

    html: added google fonts, added heading. js: completed makeGameArea. css: added styling for html/body/#header/h1/#gamesection/.row/.col. Columns and rows rigid, will try make them more dynamic later in the week.

--- js/app.js
+++ js/app.js
@@ -59,15 +59,36 @@ var checkWhoWon = function(player){
 	}
 };
 
-playerMove(playerOne,0,0);
-playerMove(playerTwo,0,1);
-playerMove(playerOne,0,2);
-// playerMove(playerTwo,1,0,"O");
-playerMove(playerOne,1,1);
-// playerMove(playerTwo,1,2,"O");
-// playerMove(playerOne,2,0,"X");
-playerMove(playerTwo,2,2);
-playerMove(playerTwo,2,1);
-playerMove(playerTwo,2,0);
+var makeGameArea = function (){
+	var gameOutline = $('<div>').attr('id','gameOutline');
+	gameOutline.appendTo($('#gameSection'));
+	///make rows
+	for (var i = 0; i < gridSize; i++) {
+		var rows = $('<div>').addClass('row '+'row'+(i+1));
+		rows.appendTo($('#gameOutline'));
+		///make columns
+		for (var j = 0; j < gridSize; j++) {
+			var cols = $('<div>').addClass('col '+'col'+(j+1));
+			cols.appendTo($('.row'+(i+1)));
+		}
+	}
+}
+makeGameArea();
+
+
+
+//////////////////////////////////
+///SAMPLE PLAY MOVES//////////////
+//////////////////////////////////
+// playerMove(playerOne,0,0);
+// playerMove(playerTwo,0,1);
+// playerMove(playerOne,0,2);
+// // playerMove(playerTwo,1,0,"O");
+// playerMove(playerOne,1,1);
+// // playerMove(playerTwo,1,2,"O");
+// // playerMove(playerOne,2,0,"X");
+// playerMove(playerTwo,2,2);
+// playerMove(playerTwo,2,1);
+// playerMove(playerTwo,2,0);
 
 });///close the jQuery listener. $(function() means the page Document Object Model (DOM) is ready for JavaScript code to execute, images/scaffolding will load before any JavaScript does.
