a9dc21b4273d8cb71148ca11b3d9056b7ace5b2b
Niall O'Callaghan <noccer@noccer-MacBook.local>
2016-05-09 12:26:49 +1000

    updated the win logic, created a 'who won' function that will work for both players

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ console.log("Welcome to Nialls Tic-Tac-Toe!");
 ///set up players and how they take a turn.
 ///
 
-var playerOne = 0; ///score
-var playerTwo = 0; ///score
+var playerOne = ["Player 1", 0]; ///name and score
+var playerTwo = ["Player 2", 0]; ///name and score
 
 var rowOneInput = ["_","_","_"];
 var rowOneValues = [1,2,4];
@@ -22,22 +22,25 @@ var winningScores = [7,56,448,73,146,292,273,84]
 var allInputsArray = [rowOneInput,rowTwoInput,rowThreeInput];
 var allValuesArray = [rowOneValues,rowTwoValues,rowThreeValues];
 
-
-var playerOneMove = function(rowNum,colNum,letter){
-	if (allInputsArray[rowNum][colNum] === "_"){
-		allInputsArray[rowNum].splice(colNum,1,letter);
-	};
-	console.table(allInputsArray);
-	playerOne = playerOne + (allValuesArray[rowNum][colNum]);
-	console.log(playerOne);
+var checkWhoWon = function(player){
 	for (var i = 0; i < winningScores.length; i++) {
-		if (playerOne === winningScores[i])
+		if (player[1] === winningScores[i])
 		{
-			console.log('playerOne wins!!!');
+			console.log(player[0]+" won!");
 		}
 	}
 };
 
-playerOneMove(2,0,"X");
-playerOneMove(1,0,"X");
-playerOneMove(0,0,"X");
+var playerOneScoreMove = function(rowNum,colNum,letter){
+	if (allInputsArray[rowNum][colNum] === "_"){
+		allInputsArray[rowNum].splice(colNum,1,letter);
+	}
+	console.table(allInputsArray);
+	playerOne[1] = playerOne[1] + (allValuesArray[rowNum][colNum]);
+	console.log(playerOne[0]+" current score is: "+playerOne[1]);
+	checkWhoWon(playerOne);
+};
+
+playerOneScoreMove(2,2,"X");
+playerOneScoreMove(1,1,"X");
+playerOneScoreMove(0,0,"X");
